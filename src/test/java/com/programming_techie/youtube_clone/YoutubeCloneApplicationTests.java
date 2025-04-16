727716207e25e5de221281a32d0e915a92577769
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-16 09:39:12 +0200

    First commit

---
+++ src/test/java/com/programming_techie/youtube_clone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming_techie.youtube_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
