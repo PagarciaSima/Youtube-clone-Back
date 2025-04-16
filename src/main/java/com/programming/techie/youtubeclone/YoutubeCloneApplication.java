1ab60ef406eee56c3fe36e184771783b8058b396
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-16 11:10:55 +0200

    Model entities

--- src/main/java/com/programming/techie/youtubeclone/YoutubeCloneApplication.java
+++ src/main/java/com/programming/techie/youtubeclone/YoutubeCloneApplication.java
@@ -1,4 +1,4 @@
-package com.programming_techie.youtube_clone;
+package com.programming.techie.youtubeclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
