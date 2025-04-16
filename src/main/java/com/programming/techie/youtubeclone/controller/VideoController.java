07da56371951f33c3e50f8ac27cbe7c95c55f54b
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-16 14:15:10 +0200

    Update video metadata

--- src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
+++ src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
@@ -2,12 +2,15 @@ package com.programming.techie.youtubeclone.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.programming.techie.youtubeclone.dto.VideoDto;
 import com.programming.techie.youtubeclone.service.VideoService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,4 +28,9 @@ public class VideoController {
 		videoService.uploadVideo(file);
 	}
 
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public VideoDto editVideoMetadata(@RequestBody VideoDto videoDto) {
+        return videoService.editVideo(videoDto);
+    }
 }
--- src/main/java/com/programming/techie/youtubeclone/service/VideoService.java
+++ src/main/java/com/programming/techie/youtubeclone/service/VideoService.java
@@ -3,6 +3,7 @@ package com.programming.techie.youtubeclone.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.programming.techie.youtubeclone.dto.VideoDto;
 import com.programming.techie.youtubeclone.model.Video;
 import com.programming.techie.youtubeclone.repository.VideoRepository;
 
@@ -22,6 +23,27 @@ public class VideoService {
     	
     	videoRepository.save(video);
     }
+
+    public VideoDto editVideo(VideoDto videoDto) {
+        // Find the video by videoId
+        var savedVideo = getVideoById(videoDto.getId());
+        // Map the videoDto fields to video
+        savedVideo.setTitle(videoDto.getTitle());
+        savedVideo.setDescription(videoDto.getDescription());
+        savedVideo.setTags(videoDto.getTags());
+        savedVideo.setThumbnailUrl(videoDto.getThumbnailUrl());
+        savedVideo.setVideoStatus(videoDto.getVideoStatus());
+
+        // save the video  to the database
+        videoRepository.save(savedVideo);
+        return videoDto;
+    }
+    
+    Video getVideoById(String videoId) {
+        return videoRepository.findById(videoId)
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find video by id - " + videoId));
+    }
+
 
    
 }
