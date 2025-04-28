5f23b146da817b460d9ee8198ba2695266089775
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-28 21:51:13 +0200

    Oauth impl

--- src/main/java/com/programming/pgs/youtubeclone/config/AudienceValidator.java
+++ src/main/java/com/programming/pgs/youtubeclone/config/AudienceValidator.java
@@ -1,5 +1,6 @@
 package com.programming.pgs.youtubeclone.config;
 
+
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
@@ -13,18 +14,6 @@ public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
         this.audience = audience;
     }
 
-    /**
-     * Validates the audience claim of the given JWT token.
-     * <p>
-     * This method checks whether the JWT token's audience contains the expected audience value.
-     * If the audience claim is valid, the validation is successful. Otherwise, it returns a failure result
-     * with an error indicating an invalid audience.
-     * </p>
-     *
-     * @param jwt the JWT token to validate
-     * @return {@link OAuth2TokenValidatorResult} representing the validation result. 
-     *         A success result if the audience is valid, or a failure result with an error if the audience is invalid.
-     */
     @Override
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         if (jwt.getAudience().contains(audience)) {
--- src/main/java/com/programming/pgs/youtubeclone/config/SecurityConfig.java
+++ src/main/java/com/programming/pgs/youtubeclone/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.programming.pgs.youtubeclone.config;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +11,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
@@ -27,38 +24,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private String audience;
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .cors( cors -> Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2ResourceServer ->
-                        oauth2ResourceServer
-                                .jwt()
-                );
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer()
+                .jwt();
     }
-    
+
+
     @Bean
     JwtDecoder jwtDecoder() {
-        // 1. Obtains a JwtDecoder based on the issuer location (OIDC issuer)
         NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
-        
-        // 2. Creates an audience validator
+
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-
-        // 3. Creates a default validator that validates the issuer of the token
         OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-
-        // 4. Creates a composite validator that validates both the issuer and the audience
         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
 
-        // 5. Sets the composite validator on the JwtDecoder
         jwtDecoder.setJwtValidator(withAudience);
 
         return jwtDecoder;
